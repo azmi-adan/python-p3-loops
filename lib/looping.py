a160af8cf37edecfca4132882f3be5ad518cc3ad
azmi adan <azmi.adan2032@gmail.com>
2024-08-27 15:46:26 +0300

    all

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # Prints a countdown from 10 to 1, then prints "Happy New Year!"
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # Returns a list of squared integers
+    return [i ** 2 for i in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    # Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+    # multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
